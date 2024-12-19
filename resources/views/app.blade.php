57f9dac8661d514ee05b051069068d616be97f5f
mjs <saputraj870@gmail.com>
2024-12-19 10:46:13 +0700

    add title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link rel="manifest" href="/manifest.json">
+    <title>Aplikasi Seserahan | MJS & L</title>
     @vite(['resources/css/app.css', 'resources/js/app.tsx'])
     @inertiaHead
     <script src="/sw-register.js"></script>
